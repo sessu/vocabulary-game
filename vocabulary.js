31f5d3013dd6509429e6442512fc4623ea6b10e5
400tacos <scource@gmail.com>
2021-02-26 00:53:44 +0900

    Split vocabulary into separate file

---
+++ vocabulary.js
@@ -0,0 +1,14 @@
+var sampleCards = [
+    ["value for money", "가격 대비 가치가 좋은"],
+    ["luxurious", "호화로운"],
+    ["timeless", "유행을 타지 않는"],
+    ["well–made", "잘 만든"],
+    ["top of the range", "최고급품의"],
+    ["durable", "내구성이 있는"],
+    ["inexpensive", "값이 싼"],
+    ["cool", "멋진"],
+    ["reliable", "믿을 수 있는"],
+    ["stylish", "멋지고 유행을 따른"],
+    ["fashionable", "유행하는"],
+    ["sexy", "섹시한"],
+];
