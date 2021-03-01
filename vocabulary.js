68750500f64a2bc59581c77b14232c9940c12c69
400tacos <scource@gmail.com>
2021-03-01 17:41:35 +0900

    Add styling, more words, shuffle problems, limit to 10 questions per round

--- vocabulary.js
+++ vocabulary.js
@@ -4,7 +4,7 @@
 // Ideally a third side would be added with a hint.
 // TODO: Find an easy way to generate this from Google Sheets, Cram, or a DB
 
-var sampleCards = [
+var sampleCards2 = [
     ["value for money", "가격 대비 가치가 좋은"],
     ["luxurious", "호화로운"],
     ["timeless", "유행을 타지 않는"],
@@ -17,4 +17,38 @@ var sampleCards = [
     ["stylish", "멋지고 유행을 따른"],
     ["fashionable", "유행하는"],
     ["sexy", "섹시한"],
+];
+
+var sampleCards = [
+    ["affordable", "저렴한", ""],
+    ["cool", "멋진", "is modern and desirable (often used by young people)"],
+    ["defective", "결함이 있는", ""],
+    ["develop", "발전시키다", ""],
+    ["durable", "내구성이 있는", "does not wear out"],
+    ["endorse", "지지하다", ""],
+    ["fashionable", "유행하는", "popular for a short time"],
+    ["inexpensive", "값이 싼", "cheap"],
+    ["launch", "출시하다", "Our company has launched a distinctive new brand."],
+    ["lifecycle of a product", "제품의 수명 주기", ""],
+    ["luxurious", "호화로운", "very expensive, comfortable, and beautiful"],
+    ["modify", "수정하다", ""],
+    ["outsource", "아웃소싱하다", ""],
+    ["place", "배치하다", ""],
+    ["promote", "(제품을) 키우다", ""],
+    ["proposition", "제의", ""],
+    ["range of products", "일련의 상품들", "We have expanded our product range."],
+    ["recall", "제품 회수", ""],
+    ["redesign", "재설계하다", ""],
+    ["relaunch", "재출시하다", ""],
+    ["reliable", "믿을 수 있는", "does not break down"],
+    ["replace", "대신하다", ""],
+    ["replacement", "대체의", ""],
+    ["roll-out", "롤아웃하다", ""],
+    ["sexy", "섹시한", "attractive in an exciting, glamorous way"],
+    ["stylish", "멋지고 유행을 따른", "attractive and has style"],
+    ["test", "시험하다", ""],
+    ["timeless", "유행을 타지 않는", "remains beautiful and does not become old-fashioned"],
+    ["top of the range", "최고급품의", "is the best of its class"],
+    ["value for money", "가격 대비 가치가 좋은", "lots of features, good quality compared to its price"],
+    ["well-made", "잘 만든", "is manufactured well and assembled to high standard"]
 ];
